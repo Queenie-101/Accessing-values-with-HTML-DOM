3f5c018dd03eb28e5033cfc647749e2e44563209
Esther <queenestheradekanm@gmail.com>
2024-11-17 18:22:40 +0100

    Accessing values with HTML DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction()
+{
+    var a=11;
+    document.getElementById("result").innerHTML = a*a
+    document.getElementById("demo").firstChild.nodeValue;
+    document.getElementById("result1").innerHTML = a+a
+    document.getElementById("demo").childNodes[0].nodeValue;
+}
